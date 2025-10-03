d43767e7ec47295fd24a360176d53ffff236595a
Gabriel Christensen <nogabrielchristensen@gmail.com>
2025-10-02 19:28:57 -0700

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,8 +1,9 @@
 const { createClient } = require("@supabase/supabase-js");
 
+// Use your Supabase URL and anon key directly if you want
 const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY
+  "https://hrkwjmdaalvxkuxbqxvu.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhya3dqbWRhYWx2eGt1eGJxeHZ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkwODQ0MDIsImV4cCI6MjA3NDY2MDQwMn0.jxpqqeeoK--xl2nTkrQVWCKZYoqs5Dpu8AeMjPbbcpE"
 );
 
 module.exports = { supabase };
